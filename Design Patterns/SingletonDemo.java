7ed50dd16af052df4de6d389e947fccc652f1df6
Your Name <kvr0908@gmail.com>
2026-07-26 20:09:05 +0530

    1.Singleton Pattern (Creational Patterns)

---
+++ Design Patterns/SingletonDemo.java
@@ -0,0 +1,25 @@
+class Singleton {
+
+    // Step 2: Private static instance (created only once)
+    private static final Singleton instance = new Singleton();
+
+    // Step 1: Private constructor
+    private Singleton() {
+        System.out.println("Singleton Instance Created");
+    }
+
+    // Step 3: Public static method to return the same instance
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2); // true
+    }
+}
